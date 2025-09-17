86ebf37303aa27fd3a31f708b949acb9e99bce23
GabrielMacedo07 <macedo425@gmail.com>
2025-09-17 12:58:48 -0300

    Estrutura inicial do projeto backend

---
+++ src/test/java/br/com/universidade/gerenciador_de_estudos/GerenciadorDeEstudosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.universidade.gerenciador_de_estudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeEstudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
